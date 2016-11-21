9b302166fbf354a5f7d91f1c4687662930a0afda
Dominic <dominic097@gmail.com>
2016-11-21 15:44:24 +0530

    _default.js -- added default basic fn

--- src/dS.js
+++ src/dS.js
@@ -1,13 +1,12 @@
 'use strict';
-
+var _default = require('./_default');
 
 /**
  * base class for dS a data Structure library build to support ML
  * copyright(c) 2016 Dominic
  * Licensed under the Apache 2.0 license. *
  */
-class dS {
-
+class dS extends _default{
 
 }
 
